a7ee0d68b52cceeb4f08cbaf0435d242944164c2
Besat Ç. <128177174+AstroBesat-SoftW@users.noreply.github.com>
2026-03-02 04:40:40 +0300

    Enhance input prompts with explanatory comments
    Added comments to clarify input expectations and variable usage.

--- Python/girdiler.py
+++ Python/girdiler.py
@@ -1,7 +1,9 @@
-
+#  Burada örnek F--AND=A,B dediysek. Burada kaç adet bilinmeyen var ve ne diye kaydettin soruyor
+# Bu örnekte A ve B bilinmeyen yani 2 adet var biri A diğeri B
 girdiler = []
 adet_2 = int(input("Kaç adet Girdi yazacaksın?\n= "))
 
 for i in range(adet_2):
     girdi = input("Girdi ne? [Orn: A,B..]\n> ")
     girdiler.append(girdi)
+
